3771c3e4d0d1d8490fc08661496442e944c98999
Filip Markovic <fma@13771>
2024-07-01 14:46:24 +0200

    Add project files.

---
+++ Ispit.Konzola/Program.cs
@@ -0,0 +1,46 @@
+﻿using Ispit.Model;
+
+namespace Ispit.Konzola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Ucenik> ucenici = new List<Ucenik>();
+
+            while (true)
+            {
+                Ucenik ucenik = new Ucenik();
+
+                Console.WriteLine("Enter ucenik name");
+                ucenik.Ime = Console.ReadLine();
+
+                Console.WriteLine("Enter ucenik prezime");
+                ucenik.Prezime = Console.ReadLine();
+
+                Console.WriteLine("Enter ucenik datum rodjenja");
+                ucenik.DatumRodjenja = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter ucenik prosjek");
+                ucenik.Prosjek = Double.Parse(Console.ReadLine());
+
+                ucenici.Add(ucenik);
+
+                Console.WriteLine("Insert new? (yes/no)");
+                if(Console.ReadLine() != "yes")
+                {
+                    break;
+                }
+            }
+
+            foreach (var ucenik in ucenici)
+            {
+                Console.WriteLine("\nIme: " + ucenik.Ime);
+                Console.WriteLine("Prezime: " + ucenik.Prezime);
+                Console.WriteLine("Starost: " + ucenik.Starost());
+                Console.WriteLine("Prosjek: " + ucenik.Prosjek);
+                Console.WriteLine("Ocjena: " + ucenik.IspisProsjeka());
+            }
+        }
+    }
+}
---
+++ Ispit.Model/Ucenik.cs
@@ -0,0 +1,27 @@
+﻿namespace Ispit.Model
+{
+    public class Ucenik
+    {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public DateTime DatumRodjenja { get; set; }
+        public double Prosjek { get; set; }
+
+        public int Starost() { 
+            DateTime today = DateTime.Today;
+            int age = today.Year - DatumRodjenja.Year;
+            if (DatumRodjenja.Date > today.AddYears(-age)) age--;
+            return age;
+        }
+        public string IspisProsjeka() {
+            return Prosjek switch
+            {
+                < 1.5 => "nedovoljan",
+                < 2.5 => "dovoljan",
+                < 3.5 => "dobar",
+                < 4.5 => "vrlo dobar",
+                _ => "odličan",
+            };
+        }
+    }
+}
